5bb18d05b7c498cffd3dcc8e7cafad22102a6748
Sandhya <sandhyagh20cl@gmail.com>
2025-02-21 10:40:16 +0530

    SingleThread added

--- obj/Debug/net8.0/Threads.AssemblyInfo.cs
+++ obj/Debug/net8.0/Threads.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Threads")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2639cbf5610d7edababe8bdddb554a664a65bee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Threads")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Threads")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
